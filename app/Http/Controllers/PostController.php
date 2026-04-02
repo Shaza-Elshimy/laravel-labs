faf87a174a1e5f0845c8f1ade4c3b4d42b16e5de
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-04-02 10:40:06 +0200

    add post model  and retrieve data from database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,24 +1,25 @@
 <?php
-
+//     [
+//     ['id'=>1,'title'=>'Learn Laravel Basics','body'=>'Introduction to routing and views in Laravel'],
+//     ['id'=>2,'title'=>'Blade Templates','body'=>'Understanding how Blade works in Laravel'],
+//     ['id'=>3,'title'=>'Forms in Laravel','body'=>'Handling form submissions and CSRF protection'],
+//     ['id'=>4,'title'=>'Working with Arrays','body'=>'Using arrays instead of database for practice'],
+//     ['id'=>5,'title'=>'Laravel Routing','body'=>'Defining routes and passing parameters'],
+//     ['id'=>6,'title'=>'Show Single Post','body'=>'Displaying one post using dynamic routes'],
+//     ['id'=>7,'title'=>'Frontend Basics','body'=>'Simple HTML and CSS for your views'],
+//     ['id'=>8,'title'=>'Debugging Errors','body'=>'How to read and fix Laravel errors'],
+//     ['id'=>9,'title'=>'Git Commits','body'=>'Writing clean and meaningful commit messages'],
+//     ['id'=>10,'title'=>'Future Steps','body'=>'Moving from arrays to database and models'],
+// ]
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
 class PostController extends Controller
 {
     function index() {
-    $posts = [
-    ['id'=>1,'title'=>'Learn Laravel Basics','body'=>'Introduction to routing and views in Laravel'],
-    ['id'=>2,'title'=>'Blade Templates','body'=>'Understanding how Blade works in Laravel'],
-    ['id'=>3,'title'=>'Forms in Laravel','body'=>'Handling form submissions and CSRF protection'],
-    ['id'=>4,'title'=>'Working with Arrays','body'=>'Using arrays instead of database for practice'],
-    ['id'=>5,'title'=>'Laravel Routing','body'=>'Defining routes and passing parameters'],
-    ['id'=>6,'title'=>'Show Single Post','body'=>'Displaying one post using dynamic routes'],
-    ['id'=>7,'title'=>'Frontend Basics','body'=>'Simple HTML and CSS for your views'],
-    ['id'=>8,'title'=>'Debugging Errors','body'=>'How to read and fix Laravel errors'],
-    ['id'=>9,'title'=>'Git Commits','body'=>'Writing clean and meaningful commit messages'],
-    ['id'=>10,'title'=>'Future Steps','body'=>'Moving from arrays to database and models'],
-];
+    $posts = Post::all();
+
     return view('posts.index',compact('posts'));
 }
 
@@ -28,31 +29,16 @@ function create() {
 }
 
 function store(Request $request) {
-    $newPost=['id'=>rand(100,999),'title'=>$request->title, 'body'=>$request->body];
+    Post::create([
+        "title"=>$request->title,
+        "body"=>$request->body
+    ]);
     return redirect('/posts');
 }
 
 function show($id) {
-    $posts = [
-    ['id'=>1,'title'=>'Learn Laravel Basics','body'=>'Introduction to routing and views in Laravel'],
-    ['id'=>2,'title'=>'Blade Templates','body'=>'Understanding how Blade works in Laravel'],
-    ['id'=>3,'title'=>'Forms in Laravel','body'=>'Handling form submissions and CSRF protection'],
-    ['id'=>4,'title'=>'Working with Arrays','body'=>'Using arrays instead of database for practice'],
-    ['id'=>5,'title'=>'Laravel Routing','body'=>'Defining routes and passing parameters'],
-    ['id'=>6,'title'=>'Show Single Post','body'=>'Displaying one post using dynamic routes'],
-    ['id'=>7,'title'=>'Frontend Basics','body'=>'Simple HTML and CSS for your views'],
-    ['id'=>8,'title'=>'Debugging Errors','body'=>'How to read and fix Laravel errors'],
-    ['id'=>9,'title'=>'Git Commits','body'=>'Writing clean and meaningful commit messages'],
-    ['id'=>10,'title'=>'Future Steps','body'=>'Moving from arrays to database and models'],
-];
-    $post=null;
+    $post = Post::find($id);
 
-    foreach($posts as $p){
-        if($p['id']==$id){
-            $post=$p;
-            break;
-        }
-    }
     return view('posts.show',compact('post'));
 }
 
