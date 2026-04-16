87f4df4c0f626523346314a93cf848c5fd1b92b9
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-04-16 14:25:07 +0200

    add authentication by create token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+;
--- routes/api.php
+++ routes/api.php
@@ -4,22 +4,41 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/{id}', [PostController::class, 'show']);
+    Route::get('/posts/{id}', [PostController::class, 'show']);
 
-Route::post('/posts', [PostController::class, 'store']);
+    Route::post('/posts', [PostController::class, 'store']);
 
-Route::post('/posts/{id}', [PostController::class, 'update']);
+    Route::post('/posts/{id}', [PostController::class, 'update']);
 
-Route::post('/posts/{id}/delete', [PostController::class, 'destroy']);
+    Route::post('/posts/{id}/delete', [PostController::class, 'destroy']);
+});
 
-
-Route::get('/users', [UserController::class, 'index']);
-
-Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+ 
+    $user = User::where('email', $request->email)->first();
+ 
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+ 
+    return $user->createToken($request->device_name)->plainTextToken;
+});
